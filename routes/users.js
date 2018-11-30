9e81f4be848d82a8349882f5f213fe6d4bd2c409
Ying Chen <jun.chenying@hotmail.com>
2018-11-30 16:32:00 +1100

    connect to mysql db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../util/mysqlConnection');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.setHeader('Content-Type', 'application/json');
+  connection.query('SELECT * from users', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({'data':results}));
+  });
 });
 
 module.exports = router;
---
+++ util/mysqlConnection.js
@@ -0,0 +1,14 @@
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : process.env.DBHOST || 'localhost',
+    user     : process.env.DBUSER || 'root',
+    password : process.env.DBPASSWORD || '',
+    database : process.env.DBNAME || 'testdb'
+  });
+
+
+connection.connect();
+
+module.exports = connection;
